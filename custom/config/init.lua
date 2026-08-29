12d0397f02577bf88c0bb8797949f3ce654786af
goddaneel <goddaneel@gmail.com>
2026-08-30 03:29:36 +0800

    fix: ftplugin

--- custom/config/init.lua
+++ custom/config/init.lua
@@ -1,24 +1,37 @@
---- path
--- and
+--- name
+-- table
 local _la_package_path = {}
+local _la_vim_runtimepath = {}
 
+
+
+--- path
+-- package.path
 _la_package_path = {
-        "/usr/lib/sayshellneovim/custom/config/?.lua",
-        "/usr/lib/sayshellneovim/custom/config/ftplugin/?.lua",
-        package.path
+        "/usr/lib/sayshellneovim/custom/config/global/?.lua",
+        package.path,
 }
 
 package.path = table.concat(_la_package_path, ";")
 
 
+-- vim.o.runtimepath
+_la_vim_runtimepath = {
+        "/usr/lib/sayshellneovim/custom/config",
+        vim.o.runtimepath,
+}
+
+vim.o.runtimepath = table.concat(_la_vim_runtimepath, ",")
+
+
 
 --- require
--- and
-require("global/01_option")
-require("global/01_keymap")
-require("global/02_highlight")
-require("global/02_filetype")
-require("global/03_toggle")
-require("global/03_config")
-require("global/03_hotkey")
-require("global/03_abbrev")
+-- global
+require("01_option")
+require("01_keymap")
+require("02_highlight")
+require("02_filetype")
+require("03_toggle")
+require("03_config")
+require("03_hotkey")
+require("03_abbrev")
